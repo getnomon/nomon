d50762ebc5bcb7ee5342d08806df363a0a22fd72
evancohen <evanbtcohen@gmail.com>
2013-02-18 00:33:27 -0800

    templating

--- dev/smart.php
+++ dev/smart.php
@@ -24,41 +24,7 @@ if($servername[0] == "dev"){
 	$s->assign("dev", '[DEV SERVER]');
 }
 
-require_once("php/lib/tamberfb.php");
-require_once("php/lib/tamberconnect.php");
-
-$tamberConnect = new TamberConnection();
-
-$smartyFB = new TamberFacebook();
-$user = $smartyFB->getUser();
-if(isset($user) && $user != 0){
-	$profile = $smartyFB->queryGraph("/me", null);
-	// TODO: Catch some expired access token stuff here and 
-	// unset user if token is expired
-	$GLOBALS['profile'] = $profile;
-}
-if($user == 0){
-	unset($user);
-}
-$GLOBALS['facebook'] = $smartyFB;
-$GLOBALS['user'] = $user;
-$GLOBALS['tamberConnection'] = $tamberConnect;
-
-
-
-if(isset($user) && $user != 0){
-	require_once("php/lib/loclog.php");
-}else{
-	$loginURL = $smartyFB->getLoginUrl(array('scope'=>'user_likes, friends_likes, user_location', 
-								'redirect_uri'=>'http://' . $_SERVER['HTTP_HOST'] . '/me',
-								'display'=>'page'));
-	$s->assign("loginURL", $loginURL);
-}
-
-$s->assign("user", $user);
-
 $template = "index";
-$country = "United States"; #can be set to true for all locations
 
 #TODO: Declare META data tags (keywords, discription, etc...)
 
@@ -70,19 +36,7 @@ if(isset($_REQUEST['q'])){
 	
 	$s->assign("Query",$arg[0],true);
 
-	switch($arg[0]){
-		case 'artist': #ARTIST;
-			$argNum = 2;
-			$name = $arg[1];
-			while(isset($arg[$argNum])){		
-				$name = $name . "/" . $arg[$argNum];
-				$argNum++;
-			}
-			$arg[1] = $name;
-			
-			$template = "artist";
-			break;
-			
+	switch($arg[0]){			
 		case '404': #404
 			$s->assign("404", $arg[0], true);
 			#load 404
@@ -102,7 +56,6 @@ if(isset($_REQUEST['q'])){
 			$inc2 = "php/inc/" . $arg[0] . ".php";
 			if(file_exists($inc2)){
 					require_once $inc2;
-					
 			}
 	}
 	#process includes
@@ -114,13 +67,6 @@ if(isset($_REQUEST['q'])){
 	if(file_exists($inc)){
 		addScript('/'.$inc);
 	}
-}else{ #homepage
-	$title = "home";
-	$template = "index";
-	if($user){
-		header( 'Location: /me');
-	}
-	$s->assign("page", "index", true);
 }
 
 function _REQUEST($arg, $index){
