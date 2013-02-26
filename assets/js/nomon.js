95e8e06bfcb1d607e9e15786a3e374b7a6ef16fc
evancohen <evanbtcohen@gmail.com>
2013-02-25 19:18:11 -0800

    I like my solution better

--- assets/js/nomon.js
+++ assets/js/nomon.js
@@ -6,30 +6,15 @@
 # REQUIREMENTS: Extreme hunger
 */
 
-if(("standalone" in window.navigator) && window.navigator.standalone){
-	// If you want to prevent remote links in standalone web apps opening Mobile Safari, change 'remotes' to true
-	var noddy, remotes = false;
-	document.addEventListener('click', function(event) {
-		noddy = event.target;
-		// Bubble up until we hit link or top HTML element. Warning: BODY element is not compulsory so better to stop on HTML
-		while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
-	        noddy = noddy.parentNode;
-	    }
-		if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)){
-			event.preventDefault();
-			document.location.href = noddy.href;
-		}
-	},false);
-}
 
 $(function() {
 	//Capture click/taps
-	/*$("a").click(function (event) {
+	$("a").click(function (event) {
 		if($(this).attr("target") != "_blank"){
 		    event.preventDefault();
 		    window.location = $(this).attr("href");
 	    }
-	});*/
+	});
 
     if($(location).attr('pathname') != '/' && $(location).attr('pathname') != '/test'){
         $('.mini-logo').attr('style', 'display: inline-block');
