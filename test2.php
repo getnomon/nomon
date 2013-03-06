ff69da070a7f8d19df76baf79fd4e82ed1f778b3
Evan Cohen <evanbtcohen@gmail.com>
2013-03-06 09:56:19 -0800

    new computer new stuff

--- test2.php
+++ test2.php
@@ -71,9 +71,8 @@ try{
     				$typeID = "NULL";
     			}
 	    		$sql = "INSERT INTO tbl_restaurant
-	    		VALUES ('$restaurant->id',  '$typeID', '$restaurant->na',' $restaurant->mino', '$address[0]', '$restaurant->cs_phone')";
-	    		echo $sql . "\n";
-	    		$query = mysqli_query($con,$sql);
+	    		VALUES ('$restaurant->id', '$typeID', '$restaurant->na',' $restaurant->mino', '$address[0]', '$restaurant->cs_phone')";
+	    		$query = mysqliQuery($con,$sql);
 	    	}
 	    }
 	    echo "<pre>";
@@ -171,7 +170,7 @@ function calcMeal($targetPrice, $result, $allergies = NULL){
 #accepts a menu id (menu parent description)
 #MUST pass menu object
 
-function getDishes($con, $rid, $item, $depth = -1){
+function getDishes($con, $rid, $item, $depth = -1, $menuid = 0, $parentid = 0){
 	#item[children] is each of the children, if it has children it is a parent. duh.
 	if(is_array($item)){
 		for ($i=0; $i < count($item); $i++) { 
@@ -184,20 +183,30 @@ function getDishes($con, $rid, $item, $depth = -1){
 			#is sub menu/item (or dish with options)
 			if($depth == 0){
 				echo "!Parent menu [$item->id] $item->name \n";
+				$menuid = $item->id;
+				$sql = "INSERT INTO tbl_menu
+	    		VALUES ('$item->id', '$item->na', '$item->descrip')";
+	    		$query = mysqliQuery($con,$sql);
 			}
+
 			for($j=0; $j<$depth; $j++){
 				echo "=";
 			}
 			echo '![' . $item->id . ']' . " $" . $item->price . " " . $item->name;
 			echo " - " . $item->descrip . "\n";
-			getDishes($con, $rid, $item->children, $depth+1);
+			getDishes($con, $rid, $item->children, $depth+1, $menuid, $item->id);
 		}else{
 			#is a dish - save shit shit
 			for($j=0; $j<$depth; $j++){
 				echo "=";
 			}
+
 			echo '[' . $item->id . ']' . " $" . $item->price . " " . $item->name;
 			echo " - " . $item->descrip . "\n";
+			$sql = "INSERT INTO tbl_menu
+	    		VALUES ('$item->id', '$menuid', '$parentid', '$item->na', '$item->descrip', '$item->price')";
+	    		//echo $sql . "\n";
+	    		$query = mysqliQuery($con,$sql);
 			//print_r($item);
 		}
 	}
@@ -205,15 +214,172 @@ function getDishes($con, $rid, $item, $depth = -1){
 
 function getRestaurantTypeID($con, $type){
 	$sql = "SELECT RestTypeID FROM tbl_restaurant_type WHERE RestTypeName='$type'";
-	$query = mysqli_query($con,$sql);
+	$query = mysqliQuery($con,$sql);
 	if($query = null){
 		$sql2 = "INSERT INTO tbl_restaurant_type (RestTypeName)
-		VALUES ($type)";
-		mysqli_query($con,$sql2);
-		$query = mysqli_query($con,$sql);
+		VALUES ('$type')";
+		mysqliQuery($con,$sql2);
+		$query = mysqliQuery($con,$sql);
 	}
 	return $query;
 }
+
+function mysqliQuery($con, $sql){
+	if(!$result = $con->query($sql)){
+    	die('There was an error running the query [' . $con->error . ']');
+	}
+}
+
+
+//Returns an array of all leaf dishes
+//Item is the parent menu object
+function buildPlatter($item, $target, $depth = -1, $price = 0, $combinations = null){
+	if($price < $target){ #Assure that possibilities 
+		if(is_array($item)){
+			for ($i=0; $i < count($item); $i++) { 
+				#Contains a bunch of stdClass Objects
+				buildPlatter($item[$i], $depth+1);
+			}
+		}else{
+			#is an stdObject -> check for children
+			if (isset($item->children)) {
+				#is sub menu/item (or dish with options)
+				if($depth == 0){
+					echo "!Parent menu [$item->id] $item->name \n";
+				}
+
+				for($j=0; $j<$depth; $j++){
+					echo "=";
+				}
+				echo '![' . $item->id . ']' . " $" . $item->price . " " . $item->name;
+				echo " - " . $item->descrip . "\n";
+				buildPlatter($item->children, $target, $depth+1, $price+$item->price);
+			}else{
+				#is a dish - save shit shit - leaf
+				for($j=0; $j<$depth; $j++){
+					echo "=";
+				}
+
+				echo '[' . $item->id . ']' . " $" . $item->price . " " . $item->name;
+				echo " - " . $item->descrip . "\n";
+				//print_r($item);
+			}
+		}
+	}
+}
+
+# Extract All Unique Conbinations
+function extractList($array, &$list, $temp = array()) {
+    if (count($temp) > 0 && ! in_array($temp, $list))
+        $list[] = $temp;
+    for($i = 0; $i < sizeof($array); $i ++) {
+        $copy = $array;
+        $elem = array_splice($copy, $i, 1);
+        if (sizeof($copy) > 0) {
+            $add = array_merge($temp, array($elem[0]));
+            sort($add);
+            extractList($copy, $list, $add);
+        } else {
+            $add = array_merge($temp, array($elem[0]));
+            sort($add);
+            if (! in_array($temp, $list)) {
+                $list[] = $add;
+            }
+        }
+    }
+}
+
+/*array_filter($list,	function($var) use ($sum) {
+		return(array_sum($var) == $sum);
+		}
+);*/
+
+#Sample Data
+/*$data = array(array(2,3,5,10,15),array(4,6,23,15,12),array(23,34,12,1,5));
+$maxsum = 25;
+print_r(bestsum($data,$maxsum));  //function call*/
+
+/*
+*	% data array of arrays or integers
+*	% maxsum caps the sum of the data
+*/
+function bestsum($data,$maxsum){
+	$res = array_fill(0, $maxsum + 1, '0');
+	$res[0] = array();              //base case
+	foreach($data as $group){
+		$new_res = $res;               //copy res
+		foreach($group as $ele){
+			for($i=0;$i<($maxsum-$ele+1);$i++){   
+				if($res[$i] != 0){
+					$ele_index = $i+$ele;
+					$new_res[$ele_index] = $res[$i];
+					$new_res[$ele_index][] = $ele;
+				}
+			}
+		}
+		$res = $new_res;
+	}
+	for($i=$maxsum;$i>0;$i--){
+		if($res[$i]!=0){
+			return $res[$i];
+		break;
+		}
+	}
+	return array();
+}
+
+#########################################################
+# 0-1 Knapsack Problem Solve (with "memo"-ization optimization)
+# $w = weight of item
+# $v = value of item
+# $i = index
+# $aW = Available Weight
+# $m = 'memo' array
+#########################################################
+
+function knapSolveFast($w,$v,$i,$aW,&$m) { // Note: We use &$m because the function writes to the $m array 
+ 
+	global $numcalls;
+	$numcalls ++;
+	// echo "Called with i=$i, aW=$aW<br>";
+ 
+	// Return memo if we have one
+	if (isset($m[$i][$aW])) {
+		return $m[$i][$aW];
+	} else {
+ 
+		if ($i == 0) {
+			if ($w[$i] <= $aW) {
+				$m[$i][$aW] = $v[$i]; // save memo
+				return $v[$i];
+			} else {
+				$m[$i][$aW] = 0; // save memo
+				return 0;
+			}
+		}	
+ 
+		$without_i = knapSolveFast($w, $v, $i-1, $aW,$m);
+		if ($w[$i] > $aW) {
+			$m[$i][$aW] = $without_i; // save memo
+			return $without_i;
+		} else {
+			$with_i = $v[$i] + knapSolveFast($w, $v, ($i-1), ($aW - $w[$i]),$m);
+			$res = max($with_i, $without_i); 
+			$m[$i][$aW] = $res; // save memo
+			return $res;
+		}	
+	}
+}
+
+################ Use Case #############################
+/*$w3 = array(1, 1, 1, 2, 2, 2, 4, 4, 4, 44, 96, 96, 96);
+$v3 = array(1, 1, 1, 2, 2, 2, 4, 4, 4, 44, 96, 96, 96);
+ 
+$numcalls = 0;
+$m = array();
+$m3 = knapSolveFast($w3, $v3, sizeof($v3) -1, 54,$m);
+print_r($w3); echo "<br>FAST: ";
+echo "<b>Max: $m3</b> ($numcalls calls)<br><br>";*/
 
 $mtime = microtime();
 $mtime = explode(" ",$mtime); 
