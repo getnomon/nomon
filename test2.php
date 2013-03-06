ac060d6b2a123565636fe9e330e7d96253ac567f
Evan Cohen <evanbtcohen@gmail.com>
2013-03-06 13:51:20 -0800

    got out of that one alive

--- test2.php
+++ test2.php
@@ -186,7 +186,7 @@ function getDishes($con, $rid, $item, $depth = -1, $parentid = 0){
 		#is an stdObject -> check for children
 		if (isset($item->children)) {
 			#is sub menu/item (or dish with options)
-			if($depth == 0){
+			if($depth == 0 && !isset($item->price)){
 				echo "!Parent menu [$item->id] $item->name \n";
 				$menuid = $item->id;
 				/*$sql = "INSERT INTO tbl_menu
