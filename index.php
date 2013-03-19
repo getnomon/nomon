41bf6823874cf23f27665a5712040e86bcfa522c
Evan Cohen <evanbtcohen@gmail.com>
2013-03-18 22:30:31 -0700

    iPhone width glitch

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ error_reporting(E_ALL);
 /*Index file makes nomon HUNGRY*/
 //Load Dependencies
 //Get Moneyn
-$SUBDOMAIN = array_shift(explode(".",$_SERVER['HTTP_HOST']));
+$SUBDOMAIN = pathinfo($_SERVER['SERVER_NAME'], PATHINFO_EXTENSION);
+
 #FORCE HTTPS
 if((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "") && $SUBDOMAIN != "dev") {
     $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
