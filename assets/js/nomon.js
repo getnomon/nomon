1d61b7ae4da84fbd5dfb1a55e468bba2a37eb7ba
Evan Cohen <evanbtcohen@gmail.com>
2013-03-19 13:54:17 -0700

    good to go live!

--- assets/js/nomon.js
+++ assets/js/nomon.js
@@ -17,7 +17,6 @@ $(function() {
 	});
 
 	$('#location').on('click', function(){
-		//http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&sensor=true_or_false
 		navigator.geolocation.getCurrentPosition(getLocation, getLocationFail);
     });
 
@@ -26,19 +25,18 @@ $(function() {
     }
 
     function getLocation(location){
-    	$.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+
-				location.coords.latitude+','+location.coords.longitude+
-				'&sensor='+((isMobile) ? 'true' : 'false'), function(data) {
-			//console.log(data);
-			//console.log('Formatted Address: ' + data.results[0].formatted_address);
+    	$.get(geoURL(location)).done(function(data) { 
 			$('#address').val(data.results[0].formatted_address);
-			alert('Is mobile: '+((isMobile) ? 'true' : 'false'));
-			//$('.result').html(data);
-			//alert('Load was performed.');
-		});
+		}).fail(function(){getLocationFail(location);});
     }
 
     function getLocationFail(location){
     	alert('Could not find your location...');
+    }
+
+    function geoURL(location){
+    	return 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+
+				location.coords.latitude+','+location.coords.longitude+
+				'&sensor='+((isMobile) ? 'true' : 'false');
     }
 });
