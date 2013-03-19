1220d5f1b6bded5116d6e9b60bd470aba729efc1
Evan Cohen <evanbtcohen@gmail.com>
2013-03-19 13:10:54 -0700

    reverse geolocation beta

--- assets/js/nomon.js
+++ assets/js/nomon.js
@@ -9,14 +9,36 @@
 
 $(function() {
 	//Capture click/taps
-	$("a").click(function (event) {
+	$("a").on('click', function (event) {
 		if($(this).attr("target") != "_blank"){
 		    event.preventDefault();
 		    window.location = $(this).attr("href");
 	    }
 	});
 
+	$('#location').on('click', function(){
+		//http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&sensor=true_or_false
+		navigator.geolocation.getCurrentPosition(getLocation, getLocationFail);
+    });
+
     if($(location).attr('pathname') != '/' && $(location).attr('pathname') != '/test'){
         $('.mini-logo').attr('style', 'display: inline-block');
+    }
+
+    function getLocation(location){
+    	$.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+
+				location.coords.latitude+','+location.coords.longitude+
+				'&sensor=false', function(data) {
+			console.log(data);
+			console.log('Formatted Address: ' + data.results[0].formatted_address);
+			$('#address').val(data.results[0].formatted_address);
+
+			//$('.result').html(data);
+			//alert('Load was performed.');
+		});
+    }
+
+    function getLocationFail(location){
+    	alert('Could not find your location...');
     }
 });
