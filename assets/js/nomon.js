12b5147836e27ab8f83b23a61a5053b3fb1822a4
Evan Cohen <evanbtcohen@gmail.com>
2013-04-03 17:32:27 -0700

    finished app prototype

--- assets/js/nomon.js
+++ assets/js/nomon.js
@@ -8,21 +8,52 @@
 
 $(function() {
 	var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|webOS)/);
+	var pathname = $(location).attr('pathname');
 	//Capture click/taps
 
+	//This is app specific code
+	if(pathname == '/app'){
+        $('.masthead').css('height', '30px');
+        $('.mini-logo').css('display', 'none');
+        //Hide rest of page inicially
+        $('#price, #allergies, #pay, #thanks, #review').css('display', 'none');
 
-	$("a").on('click', function (event) {
-		if($(this).attr("target") != "_blank"){
-		    event.preventDefault();
-		    window.location = $(this).attr("href");
-	    }
-	});
+        $('.btn').on('click', function(){
+        	var target = $(this).attr('href').substr(1);
+        	console.log('Target: ' + target);
+        	//hide all
+        	$('#index, #price, #allergies, #pay, #thanks, #review').css('display', 'none');
+        	$('.masthead').css('height', '55px');
+    		$('.mini-logo').css('display', 'inline-block');
+        	$('#'+target).css('display', 'block');
+        	return false;
+        });
+
+        $('.mini-logo a').on('click', function(){
+    		$('#price, #allergies, #pay, #thanks, #review').css('display', 'none');
+    		$('.masthead').css('height', '30px');
+    		$('.mini-logo').css('display', 'none');
+    		$('#index').css('display', 'block');
+    		return false;
+        });
+
+    }else{
+    	//Non App Code
+    	$("a").on('click', function (event) {
+			if($(this).attr("target") != "_blank"){
+			    event.preventDefault();
+			    window.location = $(this).attr("href");
+		    }
+		});
+    }
+
+    //Genaric Cross app/web code
 
 	$('#location').on('click', function(){
 		navigator.geolocation.getCurrentPosition(getLocation, getLocationFail, {enableHighAccuracy: true});
     });
 
-    if($(location).attr('pathname') != '/' && $(location).attr('pathname') != '/test'){
+    if(pathname != '/' && pathname != '/test' && pathname != "/app"){
         $('.masthead').css('height', '55px');
         $('.mini-logo').css('display', 'inline-block');
     }
@@ -53,23 +84,6 @@ $(function() {
 		fontSize = (size > 82) ? 82 + 'px':  size + 'px';
 		$('h1.title-front').css('font-size', fontSize);
 	}
-
-	//This is app specific code
-	if($(location).attr('pathname') == '/app'){
-        $('.masthead').css('height', '30px');
-        $('.mini-logo').css('display', 'none');
-        //Hide rest of page inicially
-        $('#price, #allergies, #pay, #thanks, #review').css('display', 'none');
-
-        $('.btn').on('click', function(){
-        	var target = $(this).attr('href').substr(1);
-        	console.log('Target: ' + target);
-        	//hide all
-        	$('#index, #price, #allergies, #pay, #thanks, #review').css('display', 'none');
-        	//unhide target
-        	$('#'+target).css('display', 'block');
-        	return false;
-        });
-    }
+ 
 
 });
