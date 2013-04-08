7f3eb39e516c94e17b34206603d6d6868e7fb801
Evan Cohen <evanbtcohen@gmail.com>
2013-04-07 20:59:27 -0700

    something funky with email and password validation

--- assets/js/nomon.js
+++ assets/js/nomon.js
@@ -186,5 +186,6 @@ function api(type){
 
 function validateEmail(email) { 
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    console.log('validating email...');
     return re.test(email);
 } 
